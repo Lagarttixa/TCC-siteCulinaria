690d3c1eb267b5c631f7507bcdd4752a9edd57b1
Lagarttixa <abner.cars@hotmail.com>
2022-07-26 11:57:03 -0300

    inicio

---
+++ src/main/java/com/tcc/siteculinariaapi/SiteculinariaApiApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.siteculinariaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteculinariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteculinariaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/siteculinariaapi/SiteculinariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.siteculinariaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteculinariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
